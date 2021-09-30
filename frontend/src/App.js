d1914b48a76f8a82a3a2d1761dca5b9dcd7bf379
Marcus Davi Argolo Ribeiro <marcusdavi@gmail.com>
2021-09-30 19:05:48 -0300

    start configurarion

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+export default function App() {
+  console.log('Teste no console do navegador');
+
+  return (
+    <div>
+      <header>
+        <div className="bg-gray-100 mx-auto p-4">
+          <h1 className="text-center font-semibold text-xl">
+            React Brasileirao
+          </h1>
+        </div>
+      </header>
+
+      <main>
+        <div className="container mx-auto p-4">
+          <h2>O conteúdo fica aqui.</h2>
+        </div>
+      </main>
+    </div>
+  );
+}
